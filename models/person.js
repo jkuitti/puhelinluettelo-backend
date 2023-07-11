9f0c5f565bfc06be03caee0ade87b0091311f5df
Joonas Kuittinen <jokuitti@student.uef.fi>
2023-07-12 02:15:28 +0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -3,42 +3,42 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
-  
+
 console.log('Connecting to', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log('Connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('Error connecting to MongoDB', error.message)
-    })
+  .then(result => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minlength: [3, 'Name must be at least 3 characters long'],
     required: [true, 'User name required']
-  }, 
+  },
   number:{
     type: String,
     minlength: [8, 'Number must be at least 8 digits long'],
     validate: {
-        validator: (v) => {
-            return /\d{2,3}-\d{6,}/.test(v);
-        },
-        message: props => `${props.value} is not a valid phone number`
+      validator: (v) => {
+        return /\d{2,3}-\d{6,}/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number`
     },
     required: [true, 'User phone number required']
   }
-});
+})
 
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
